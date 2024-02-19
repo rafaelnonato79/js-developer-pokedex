43bab6149e70d172e1c7a3d2f9d39e4afc4289cd
Rafael Nonato <rafaelnonato79@gmail.com>
2024-02-19 20:20:51 -0300

    adicionado estilo de cor aos cards de acordo com o type do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,16 @@ function fecharDialogo() {
 function mostrarDialogo(event) {
     const pokemonName = event.currentTarget.querySelector('.name').textContent;
     const pokemonNumber = event.currentTarget.querySelector('.number').textContent;
-    const pokemonTypes = event.currentTarget.querySelectorAll('.types li');
+    const pokemonType = event.currentTarget.querySelector('.types li:first-child').textContent; // Aqui pegamos apenas o primeiro tipo
     const pokemonPhoto = event.currentTarget.querySelector('.detail img').src;
+    const pokemonTypesHTML = event.currentTarget.querySelector('.types').innerHTML;
 
-    
+    // Converter a string HTML em elementos DOM
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = pokemonTypesHTML;
 
+    // Obter uma lista de tipos de Pokémon
+    const pokemonTypes = tempElement.querySelectorAll('li');
 
     const dialog = document.getElementById('dialogo');
     const dialogPokemonName = dialog.querySelector('.pokemonName');
@@ -69,14 +74,22 @@ function mostrarDialogo(event) {
     const dialogPokemonTypes = dialog.querySelector('.pokemonTypes');
     const dialogPokemonPhoto = dialog.querySelector('.pokemonPhoto');
 
-     // Limpar qualquer conteúdo anterior
-     dialogPokemonTypes.innerHTML = '';
+    // Limpar qualquer conteúdo anterior
+    dialogPokemonTypes.innerHTML = '';
+
+    // Adicionar classes ao diálogo com base no tipo de Pokémon
+    dialog.classList.remove('grass', 'fire', 'water', 'bug', 'normal', 'eletric', 'ice', 'ground', 'flying', 'poison',
+    'fighting', 'psychic', 'rock', 'ghost', 'dark', 'dragon', 'steel', 'fairy'
+    ); // Limpar classes existentes
+    dialog.classList.add(pokemonType.toLowerCase()); // Adiciona a classe correspondente ao tipo do Pokémon
 
     // Exibir o nome do Pokémon no diálogo
     dialogPokemonName.textContent = pokemonName;
     dialogPokemonNumber.textContent = pokemonNumber;
     dialogPokemonPhoto.src = pokemonPhoto;
-    pokemonTypes.forEach((type) => {
+
+    // Adicionar cada tipo à lista de tipos no diálogo
+    pokemonTypes.forEach(type => {
         const li = document.createElement('li');
         li.textContent = type.textContent;
         dialogPokemonTypes.appendChild(li);
@@ -84,4 +97,6 @@ function mostrarDialogo(event) {
 
     dialog.showModal();
 }
+
+
 
